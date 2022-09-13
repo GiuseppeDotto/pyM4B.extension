aa87041dc07914bb7d386b5d55d4e795fdd5ded3
Giuseppe Dotto <gppdtt@gmail.com>
2022-09-13 08:02:24 +0200

    Rename files - update

--- pyM4B.tab/COO.panel/Rename FIles.pushbutton/RenameFiles_script.py
+++ pyM4B.tab/COO.panel/Rename FIles.pushbutton/RenameFiles_script.py
@@ -17,17 +17,20 @@ from rpw.ui.forms import (FlexForm, Label, TextBox, CheckBox,\
 
 
 def find_between(txt, start, end):
-	indices_start = [n for i,n in enumerate(txt) if i==start]
-	indices_end = [n for i,n in enumerate(txt) if i==end]
+	out = []
+	indices_start = [n for n,i in enumerate(txt) if i==start]
+	indices_end = [n for n,i in enumerate(txt) if i==end]
 
 	for s,e in zip(indices_start, indices_end):
-		yield txt[s:e] +end
+		out.append( txt[s:e] +end )
+	return	out
 
 def get_par_value(par):
 	if par.StorageType == DB.StorageType.String:
 		return	par.AsString()
 	else:
 		return	par.AsValueString()
+
 
 doc = revit.doc
 
@@ -40,12 +43,13 @@ components = [Label('New name format:'),
 			TextBox('textbox1', Text="M4B_{Project Parameter}_[Sheet Parameter]"),
 			Separator(),
 			CheckBox('archive', 'Create Archive', True),
+			CheckBox('subFolder', 'Rename files in sub-folders', False),
 			Separator(),
 			Button('OK')]
 
 form = FlexForm('Rename files in forlder', components)
 form.show()
-forms.alert_ifnot(form.values['textbox1'], 'No Name defined', exitscript=True)
+forms.alert_ifnot(form.values.get('textbox1'), 'No Name defined', exitscript=True)
 
 
 # GET ALL SHEETS IN THE MODEL
@@ -56,41 +60,58 @@ all_sheets = dict( [[s.SheetNumber, s] for s in all_sheets] )
 proj_info = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_ProjectInformation).FirstElement()
 
 
-# define back-up folder
-now = datetime.now()
-archive_folder = now.strftime('%y%m%d - %H%M') + ' - Archive'
-archive_folder = os.path.join(dest_dir, archive_folder)
+if form.values['archive']:
+	# define back-up folder
+	now = datetime.now()
+	archive_folder = now.strftime('%y%m%d - %H%M') + ' - Archive'
+	archive_folder = os.path.join(dest_dir, archive_folder)
+	os.makedirs(archive_folder)
+
+not_allowed = '\/:*?"<>|'
 
 for dirpath, dirnames, filenames in os.walk(dest_dir):
 	filenames = [f for f in filenames if any([x in f for x in all_sheets.keys()])]
+	new_filenames = []
 	for f in filenames:
-		
 		if form.values['archive']:
 			shutil.copy2(os.path.join(dirpath, f), os.path.join(archive_folder, f))
 
-
+		new_name = form.values.get('textbox1')[:]
 		sheet = [s_nr for s_nr in all_sheets.keys() if s_nr in f]
 		sheet = sorted(sheet, key=lambda n:len(n))[-1]
 		sheet = all_sheets[sheet]
 		# Replace Sheet Parameters
-		for sh_par in find_between(f, '[', ']'):
+		for sh_par in find_between(new_name, '[', ']'):
 			par = sheet.LookupParameter(sh_par[1:-1])
 			forms.alert_ifnot(par, "{} parameter not recognized.".format(sh_par), exitscript=True)
 			new_text = get_par_value(par)
 			
-			f.replace(sh_par, new_text)
+			new_name = new_name.replace(sh_par, new_text)
 		
 		# Replace Project Parameter
-		for pr_par in find_between(f, '{', '}'):
+		for pr_par in find_between(new_name, '{', '}'):
 			par = proj_info.LookupParameter(pr_par[1:-1])
 			forms.alert_ifnot(par, "{} parameter not recognized.".format(pr_par), exitscript=True)
 			new_text = get_par_value(par)
 			
-			f.replace(pr_par, new_text)
-
+			new_name = new_name.replace(pr_par, new_text)
 		
+		new_filenames.append(new_name+'.{}'.format(f.split('.')[-1]))
 
+
+	# CHECK THE SPECIAL CHARACTERS
+	if sum([x in ''.join(new_filenames) for x in not_allowed]):
+		forms.alert('{}\nCharacter not permitted will be replaced with "-".'.format(not_allowed))
 		
+	forms.alert('Example file name:\n{}\n\nContinue?'.format(new_filenames[0]),
+					yes=True, no=True, exitscript=True)
+	
+	
+	for old, new in zip(filenames, new_filenames):
+		print([old, new])
+
+	if not form.values['subFolder']:
+		break
 
 
 
