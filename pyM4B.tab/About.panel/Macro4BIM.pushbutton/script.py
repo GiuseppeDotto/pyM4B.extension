8f9e07bb203849001367fa35a1eac54c0721fc29
Giuseppe Dotto <gppdtt@gmail.com>
2023-03-21 00:27:42 +0100

    about button more interactive

--- pyM4B.tab/About.panel/Macro4BIM.pushbutton/script.py
+++ pyM4B.tab/About.panel/Macro4BIM.pushbutton/script.py
@@ -2,5 +2,8 @@
 import webbrowser
 from pyrevit import forms
 
-if forms.alert("Thanks for using our scripts!", warn_icon=False, options=['go to website', 'bye!']) == 'go to website':
+user_input = forms.alert("Thanks for using our scripts!", warn_icon=False, options=['Go to website', 'Share ideas.', 'Ciao!'])
+if user_input == 'Go to website':
 	webbrowser.open('https://www.macro4bim.com')
+elif user_input == 'Share ideas.':
+	webbrowser.open("https://www.macro4bim.com/#custom-solutions")
