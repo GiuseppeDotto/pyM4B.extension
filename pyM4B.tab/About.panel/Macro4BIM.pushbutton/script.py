b7527321c52e13eca6af77279ec13661b070a423
Giuseppe Dotto <gppdtt@gmail.com>
2023-03-20 22:15:07 +0100

    correct about button

--- pyM4B.tab/About.panel/Macro4BIM.pushbutton/script.py
+++ pyM4B.tab/About.panel/Macro4BIM.pushbutton/script.py
@@ -2,6 +2,5 @@
 import webbrowser
 from pyrevit import forms
 
-forms.alert("Thanks for using our scripts!", warn_icon=False)
-
-webbrowser.open('https://www.macro4bim.com')
+if forms.alert("Thanks for using our scripts!", warn_icon=False, options=['go to website', 'bye!']) == 'go to website':
+	webbrowser.open('https://www.macro4bim.com')
