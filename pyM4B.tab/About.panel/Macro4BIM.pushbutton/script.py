25cafe867ea5f5bf55ba2f6b323701b5b6711e3b
Giuseppe Dotto <43936238+GiuseppeDotto@users.noreply.github.com>
2023-08-16 00:41:10 +0200

    test vsc repositories from vscode

--- pyM4B.tab/About.panel/Macro4BIM.pushbutton/script.py
+++ pyM4B.tab/About.panel/Macro4BIM.pushbutton/script.py
@@ -2,6 +2,7 @@
 import webbrowser
 from pyrevit import forms
 
+
 user_input = forms.alert("Thanks for using our scripts!", warn_icon=False, options=['Go to website', 'Share ideas.', 'Ciao!'])
 if user_input == 'Go to website':
 	webbrowser.open('https://www.macro4bim.com')
