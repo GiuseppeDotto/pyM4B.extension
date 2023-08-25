a69902858fb4ebb964a36e0c4661baae7ba8f5da
Giuseppe Dotto <43936238+GiuseppeDotto@users.noreply.github.com>
2023-08-25 16:27:07 +0200

    Update AutoLegend_script.py

--- pyM4B.tab/ARC.panel/Auto-Legend.pushbutton/AutoLegend_script.py
+++ pyM4B.tab/ARC.panel/Auto-Legend.pushbutton/AutoLegend_script.py
@@ -2,118 +2,149 @@
 # copyright 2023, Macro4BIM (www.macro4bim.com)
 # Author: Giuseppe Dotto
 
-from pyrevit import DB, forms, revit, script
+from pyrevit import script, forms, revit, DB
+from rpw.ui.forms import FlexForm, Label, ComboBox, Separator, Button
 # import DB
-
-__title__ = 'Auto\nLegend'
 
 doc = revit.doc
 vw = doc.ActiveView
 
-def get_layers(elem):
-	w_type = elem.get_Parameter(DB.BuiltInParameter.LEGEND_COMPONENT).AsElementId()
+tag_distribution = lambda l: [sum(l[:n])+l[n]/2.0 for n in range(len(l))]
+def get_layers(legend_component):
+	"""
+	INPUT: legend_component\n
+	OUTPUT: [all_widths, material_ids]
+	"""
+	w_type = legend_component.get_Parameter(DB.BuiltInParameter.LEGEND_COMPONENT).AsElementId()
 	w_type = doc.GetElement(w_type)
 	
 	all_width = []
-	all_positions = []
-	progressive = 0
+	materials_id = []
 	if w_type.GetCompoundStructure():
 		for l in reversed(w_type.GetCompoundStructure().GetLayers()):
 			if l.Width: 
-				progressive += l.Width
-				all_positions.append(progressive - l.Width/2)
 				all_width.append( l.Width )
-	return all_positions, all_width
+				materials_id.append( l.MaterialId )
+	return all_width, materials_id
 
-
-def get_basePt(elem):
-	widths = get_layers(elem)[1]
-	bb = elem.get_BoundingBox(vw)
-	base = (bb.Min.X+bb.Max.X)/2 - sum(widths)/2
-	return	DB.XYZ(base, bb.Min.Y, 0)
-
-def material_tag(base, elem, tag_id):
-	y = 0.2
-	for n, x in enumerate(get_layers(elem)[0]):
-		pt = base.Add(DB.XYZ(x,y*(n+1),0))
-		new_tag = DB.IndependentTag.Create( doc, tag_id, vw.Id,
-							DB.Reference(elem), True,
-							DB.TagOrientation.Horizontal, pt)
-		
-		new_tag.SetLeaderEnd(DB.Reference(elem), pt)
-		new_tag.TagHeadPosition = DB.XYZ(elem.get_BoundingBox(vw).Max.X + 0.5, pt.Y, 0)
-		
-
-
-def create_dimension(base, elem):
-	ln_base = DB.Line.CreateBound(base, base.Add(DB.XYZ(0,0.1,0)))
+def create_dimension_horizontal(legend_component, widths, horizontal=True):
+	if horizontal:
+		bb = legend_component.get_BoundingBox(vw)
+		base = bb.Min
+		tot_len = bb.Max.X - bb.Min.X
+		vec = DB.XYZ(0,1,0)
+		ln_base = DB.Line.CreateBound(base, base.Add(DB.XYZ(0.1,0,0)))
+	else:
+		bb = legend_component.get_BoundingBox(vw)
+		base = bb.Min
+		tot_len = bb.Max.Y - bb.Min.Y
+		vec = DB.XYZ(1,0,0)
+		ln_base = DB.Line.CreateBound(base, base.Add(DB.XYZ(0,0.1,0)))
 	dt_crv = doc.Create.NewDetailCurve(vw, ln_base)
-	
-	widths = get_layers(elem)[1]
-	progression = 0
-	
 	ref_array_total = DB.ReferenceArray()
 	ref_array_total.Append(DB.Reference(dt_crv))
 	ref_array = DB.ReferenceArray()
 	ref_array.Append(DB.Reference(dt_crv))
+	progression = 0
 	for w in widths:
 		progression += w
-		new_crv = DB.ElementTransformUtils.CopyElement(doc, dt_crv.Id, DB.XYZ(progression,0,0))
+		new_crv = DB.ElementTransformUtils.CopyElement(doc, dt_crv.Id, vec.Multiply(progression))
 		ref_array.Append( DB.Reference(doc.GetElement(new_crv[0])) )
 	ref_array_total.Append( DB.Reference(doc.GetElement(new_crv[0])) )
 
-	dim_ln = DB.Line.CreateUnbound(base.Add(DB.XYZ(0,3.5,0)), DB.XYZ(1,0,0))
+	dim_ln = DB.Line.CreateUnbound(base.Add(vec.CrossProduct(DB.XYZ(0,0,-1)).Multiply(tot_len+0.25)), vec)
 	doc.Create.NewDimension(vw, dim_ln, ref_array)
 	
-	dim_ln = DB.Line.CreateUnbound(base.Add(DB.XYZ(0,3.8,0)), DB.XYZ(1,0,0))
+	dim_ln = DB.Line.CreateUnbound(base.Add(vec.CrossProduct(DB.XYZ(0,0,-1)).Multiply(tot_len+0.5)), vec)
 	doc.Create.NewDimension(vw, dim_ln, ref_array_total)
 
+# SET USER INPUT
+phases = DB.FilteredElementCollector(doc).OfClass(DB.Phase)
+phases = dict( [[p.Name, p.Id] for p in phases] )
+components = [Label('LEGEND COMPONENT ORIENTATION'),
+			  ComboBox('orientation', ['Veritcal', 'Horizontal']),
+			  Label('REFERENCE PROJECT PHASE. (where elements are created)'),
+			  ComboBox('phase', phases.keys()),
+			  Separator(),
+			  Button('OK')]
+form = FlexForm('Auto-Legend Components', components)
+form.show()
+if not form.values.get('orientation'):  script.exit()
 
 
-def create_title(base, elem, ref_text):
-	# base = base.Add( XYZ(sum(get_layers(elem)[1])/2,0,0) ) # move to center
-	w_type = elem.get_Parameter(DB.BuiltInParameter.LEGEND_COMPONENT).AsElementId()
-	w_type = doc.GetElement(w_type)
-	
-	par = w_type.LookupParameter( ref_text.Text[:-1] )
-	if par:
-		value = par.AsString()
-		if not value:   value = '-'
-		DB.TextNote.Create(doc, vw.Id, base.Add(DB.XYZ(0,-0.2,0)), value, ref_text.GetTypeId())
+with forms.WarningBar(title='SELECT A LEGEND COMPONENT'):
+	legend = revit.pick_element()
+	forms.alert_ifnot(legend.Category.BuiltInCategory == DB.BuiltInCategory.OST_LegendComponents,
+					  'Select a Legend Component not selected.', exitscript=True)
+with forms.WarningBar(title='SELECT THE MATERIAL TAG TO USE'):
+	m_tag = revit.pick_element_by_category(DB.BuiltInCategory.OST_MaterialTags)
+with forms.WarningBar(title='SELECT TEXT-NOTES TO READ AND COPY'):
+	id_text = revit.pick_element_by_category(DB.BuiltInCategory.OST_TextNotes)
+	par_name = id_text.Text[:-1]
+	print('-'+par_name+'-')
+	# script.exit()
 
 
-# CHECK YOU ARE IN LEGEND VIEW
-forms.alert_ifnot(vw.ViewType == DB.ViewType.Legend, 'The function only works in Legend View.', exitscript=True )
+# COLLECT TYPES TO ADD
+phase_filetr = DB.ElementPhaseStatusFilter(phases[form.values['phase']],DB.ElementOnPhaseStatus.New)
+cat_id = doc.GetElement(legend.get_Parameter(DB.BuiltInParameter.LEGEND_COMPONENT).AsElementId()).Category.Id
+elems = DB.FilteredElementCollector(doc).OfCategoryId(cat_id).WhereElementIsNotElementType().WherePasses(phase_filetr)
+elems = set([e.GetTypeId() for e in elems])
+elems = set( [e for e in elems if 'curtain' not in doc.GetElement(e).FamilyName.lower()] )
+# DEFINE FIXED TRANSLATION
+translation = legend.get_BoundingBox(vw)
+pt_base = translation.Min
+translation = (translation.Max.X - translation.Min.X)*2
 
-# COLLECT INPUT
-with forms.WarningBar(title='SELECT LEGEND COMPONENT'):
-	source_component = revit.pick_element()
-	if not source_component: script.exit()
-	forms.alert_ifnot(source_component.Parameter[DB.BuiltInParameter.LEGEND_COMPONENT],
-					"Select a Legend Component", exitscript=True)
-with forms.WarningBar(title='SELECT MATERIAL TAG'):
-	source_tag = revit.pick_element_by_category(DB.BuiltInCategory.OST_MaterialTags)
-	if not source_tag: script.exit()
-with forms.WarningBar(title='SELECT TEXT TYPE WITH PARAMETER NAME'):
-	ref_text = revit.pick_element_by_category(DB.BuiltInCategory.OST_TextNotes)
-	if not ref_text: script.exit()
+amount = len(elems)
+with revit.TransactionGroup('Automatic Legend'):
+	for x, e_type in enumerate(elems):
+		with revit.Transaction('Copy and change Type'):
+			new_elem = DB.ElementTransformUtils.CopyElement(doc, legend.Id, DB.XYZ(translation*(x+1),0,0))
+			new_elem = doc.GetElement(new_elem[0])
+			new_elem.get_Parameter(DB.BuiltInParameter.LEGEND_COMPONENT).Set(e_type)
+			print(new_elem)
 
-all_wallType_ids = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Walls).WhereElementIsNotElementType()
-all_wallType_ids = set( [w.WallType.Id for w in all_wallType_ids] )
-all_wallType_ids = [wt for wt in all_wallType_ids if doc.GetElement(wt).GetCompoundStructure()]
+		with revit.Transaction('place tags'): 
+			ref = DB.Reference(new_elem)
+			bb = new_elem.get_BoundingBox(vw)
 
+			pts = []
+			layers_info = get_layers(new_elem)
+			spacing = 0.25
+			pt = bb.Min
+			for n, y in enumerate(tag_distribution(layers_info[0])):
+				if form.values['orientation'] == "Horizontal":
+					new_pt = pt.Add( DB.XYZ((n+1)*spacing, y, bb.Min.Z) )
+					pts.append(new_pt)
+				else:
+					new_pt = pt.Add( DB.XYZ(y, (n+1)*spacing, bb.Min.Z) )
+					pts.append(new_pt)
+			if form.values['orientation'] == "Horizontal":
+				Xs = [p.X for p in pts]
+				for i, x in enumerate(Xs[::-1]):
+					pts[i] = pts[i] = DB.XYZ(x, pts[i].Y, pts[i].Z)
+			for n, pt in enumerate(pts):
+				new_tag = DB.IndependentTag.Create(doc, m_tag.GetTypeId(), vw.Id, ref,
+										True, DB.TagOrientation.Horizontal, pt)
+				
+				new_tag.LeaderEndCondition = DB.LeaderEndCondition.Free
+				new_tag.SetLeaderEnd(ref,pt)
+				if form.values['orientation'] == "Horizontal":
+					new_tag.SetLeaderElbow(ref, DB.XYZ(pt.X, bb.Max.Y + (n+1)*spacing, pt.Z))
+					new_tag.TagHeadPosition = DB.XYZ(bb.Max.X, bb.Max.Y +(n+1)*spacing, pt.Z)
+				else:
+					new_tag.SetLeaderElbow(ref, DB.XYZ(bb.Max.X, pt.Y, pt.Z))
+					new_tag.TagHeadPosition = DB.XYZ(bb.Max.X+spacing, pt.Y, pt.Z)
 
-spacing = 3.5
-with revit.Transaction('Automatic Walls Legend'):
-	for n, wall_t in enumerate(all_wallType_ids):
-		new_elem = DB.ElementTransformUtils.CopyElement(doc, source_component.Id, DB.XYZ(spacing + spacing*n,0,0))
-		new_elem = doc.GetElement(new_elem[0])
-		new_elem.get_Parameter(DB.BuiltInParameter.LEGEND_COMPONENT).Set(wall_t)        
-		
-		base = get_basePt(new_elem)
-		
-		material_tag(base, new_elem, source_tag.GetTypeId())
-		create_dimension(base, new_elem)
-		create_title(base, new_elem, ref_text)
-		
+		with revit.Transaction('Create Dimensions'):
+			if form.values['orientation'] == "Horizontal":
+				create_dimension_horizontal(new_elem, layers_info[0], horizontal=True)
+			else:
+				create_dimension_horizontal(new_elem, layers_info[0], horizontal=False)
 
+		with revit.Transaction('CopyPaste and create text'):
+			vec = bb.Min.Subtract(pt_base)
+			new_txt = DB.ElementTransformUtils.CopyElement(doc, id_text.Id, vec)
+			new_txt = doc.GetElement(new_txt[0])
+			new_txt.Text = str(doc.GetElement(e_type).LookupParameter(par_name).AsValueString())
