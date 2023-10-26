f5e6ced181b3c2e9fd069b6e34cff7f51cbe9985
Giuseppe Dotto <gppdtt@gmail.com>
2023-10-26 18:27:48 +0200

    rename folder

--- pyM4B.tab/rename folder.py
+++ pyM4B.tab/rename folder.py
@@ -6,8 +6,8 @@ def rename_nested(folder, addition, enum):
     for dirpath, dirnames, filenames in os.walk(folder):
         for d in dirnames:
             complete_dir = os.path.join(dirpath, d)
-            new_path = complete_dir+addition
-            # new_path = complete_dir.replace(addition,'')
+            # new_path = complete_dir+addition
+            new_path = complete_dir.replace(addition,'')
             os.rename(complete_dir, new_path)
             enum += 1
             rename_nested(new_path, addition, enum)
