89df3dad337f0c0629f69d593c406136ccc17906
Giuseppe Dotto <gppdtt@gmail.com>
2024-08-24 19:25:38 +0200

    update find&replace function

--- pyM4B.tab/Modify.panel/renaming.stack/Find&Replace.pushbutton/findAndReplace_script.py
+++ pyM4B.tab/Modify.panel/renaming.stack/Find&Replace.pushbutton/findAndReplace_script.py
@@ -1,79 +1,36 @@
-__doc__ = "Find and replace given text to the Family or the Type names "\
-			"of the selected elements.\nTip: you can also select Types from "\
-			"the Project Browser window."
+from pyrevit import script, revit, DB, forms
+try: import DB
+except: pass
+from rpw.ui.forms import FlexForm, Label, TextBox, Separator, Button
 
-
-from pyrevit import script, revit, DB
-from rpw.ui.forms import (FlexForm, Label, ComboBox, TextBox,\
-						Separator, Button, TaskDialog, Alert)
-
-
-def ask_confirmation(lst, obj):
-	dialog = TaskDialog('Rename {} {}?'.format(len(lst), obj), buttons=['Yes', 'No'])
-	if dialog.show() == 'No':
-		script.exit()
-
-selection = revit.get_selection()
-if len(selection) == 0:
-	Alert('Select at least one element or type.',
-		header='No elements have been selected.',
-		exit=True)
+doc = revit.doc
 
 # CREATE INPUT FORM
-components = [Label('Select elements of the same'),
-			ComboBox('combobox1', ['Family Name', 'FamilyType or Views Name']),
-			Separator(),
-			Label('Search for:'),
-			TextBox('textbox1', Text="search"),
-			Label('Replace with:'),
-			TextBox('textbox2', Text="replace"),
-			Separator(),
-			Button('OK')]
+components = [Label('Search for:'),
+			  TextBox('old', Text="search"),
+			  Label('Replace with:'),
+			  TextBox('new', Text="replace"),
+			  Separator(),
+			  Button('OK')]
 
-form = FlexForm('Choose what to rename', components)
+form = FlexForm('Find and Replace', components)
 form.show()
 
-# SCRIPT
-BIP = DB.BuiltInParameter
-typ_name = lambda x: x.Parameter[BIP.SYMBOL_NAME_PARAM].AsString()
-accepted_classes = [DB.WallType, DB.FloorType,
-					DB.CeilingType, DB.RoofType,
-					DB.SlabEdgeType]
+old_text = form.values.get('old')
+new_text = form.values.get('new')
+if not old_text: script.exit()
 
-if form.values['combobox1'] == 'Family Name':
-	selected_fam = []
-	for e in selection:
-		if e.GetType() == DB.FamilySymbol:
-			selected_fam.append(e.Family.Id)
-		else:
-			selected_fam.append(e.Symbol.Family.Id)
-	selected_fam = set(selected_fam)
+selection = revit.get_selection()
+amount = 0
+with revit.Transaction('M4B - FInd & Replace'):
+	for elem in selection:
+		current_name = DB.Element.Name.__get__(elem)
+		if old_text in current_name:
+			new_name = current_name.replace(old_text, new_text)
+			if hasattr(elem, 'ViewType') or hasattr(elem, 'FamilyName'):
+				elem.Name = new_name
+			else:
+				doc.GetElement(elem.GetTypeId()).Name = new_name
+			amount += 1
 
-	ask_confirmation(selected_fam, 'Families')
-	with revit.Transaction('Rename Families'):	
-		for f in selected_fam:
-			f = revit.doc.GetElement(f)
-			f.Name = f.Name.replace(form.values['textbox1'], form.values['textbox2'])
-
-elif form.values['combobox1'] == 'FamilyType or Views Name':
-	accepted_views = [DB.ViewPlan, DB.ViewSection, DB.ViewSchedule,
-						DB.ViewSheet, DB.View3D, DB.View]
-	selected_type = []
-	for e in selection:
-		#if e.GetType() == DB.FamilySymbol:
-		if any([e.GetType() == x for x in accepted_classes]):
-			selected_type.append(e.Id)
-		elif any([e.GetType() == x for x in accepted_views]):
-			selected_type.append(e.Id)
-		else:
-			selected_type.append(e.Symbol.Id)
-	selected_type = set(selected_type)
-	
-	ask_confirmation(selected_type, 'Types')
-	with revit.Transaction('Rename Types'):
-		for t in selected_type:
-			t = revit.doc.GetElement(t)
-			t.Name = DB.Element.Name.__get__(t).replace(form.values['textbox1'],
-										form.values['textbox2'])
-
-
+forms.alert_ifnot(amount==0, '{} elements renamed.'.format(amount), warn_icon=False)
